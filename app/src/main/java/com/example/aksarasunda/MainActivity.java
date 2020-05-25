8845436fe244a03e973a3cbd72941bc5dafdbd3c
irfan44 <irfannmuhajir@gmail.com>
2020-05-25 09:58:03 +0700

    Add layout "Aksara Ngalagena"

--- app/src/main/java/com/example/aksarasunda/MainActivity.java
+++ app/src/main/java/com/example/aksarasunda/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.aksarasunda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import androidx.cardview.widget.CardView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ngalagena (View view){
+        Intent intent = new Intent(MainActivity.this, menu_ngalagena.class);
+        startActivity(intent);
     }
 
 
